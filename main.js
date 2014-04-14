e9b8eae34dd9adc4a88ef56357d4eccf395b6e68
Sean Manhertz <sean.manhertz@gmail.com>
2014-04-14 17:53:17 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,55 @@
+$(document).on('ready', function() {
+  
+});
+// var tripleFive = function tripleFive(num) {
+// 	var rtn = ["Five!"];
+// 	for (var i = 0; i < num; i++) {
+// 		num += i;
+// 		};
+// 	return rtn;
+// }
+
+// console.log(tripleFive(3))
+
+var lastLetter = function lastLetter(str) {
+
+}
+
+var square = function square(n) {
+	return n * n;
+}
+
+console.log(square(4))
+
+var negate= function negate(n) {
+	return -(n);
+}
+console.log (negate(7))
+
+var toArray = function toArray(a,b,c) {
+	return ;
+}
+console.log (toArray(1,2,3))
+
+var excite = function excite (str) {
+	return str + "!!!";
+}
+
+console.log (excite('this is freaking bananas'))
+
+var sun= function sun(str) {
+	return str.indexOf('sun');
+}
+
+console.log (sun('sun'))
+
+var tiny = function tiny(n) {
+	if (n >= 0 && n <= 1) {
+		return true;
+	}
+	else {
+		return false;
+	} ;
+};
+
+console.log (tiny(0))
